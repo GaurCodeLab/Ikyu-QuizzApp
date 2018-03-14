312a5974272b26038f3fd133a6680300bd78ef56
ankit gaur <ankit.gaur1023@gmail.com>
2018-03-14 17:57:49 +0530

    quizz activity

--- app/src/main/java/com/ankitgaur/ikyu/CategoryActivity.java
+++ app/src/main/java/com/ankitgaur/ikyu/CategoryActivity.java
@@ -1,15 +1,27 @@
 package com.ankitgaur.ikyu;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CategoryActivity extends AppCompatActivity {
+
+    private String mDisplayName;
+    private DatabaseReference mDatabaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        setupDisplayName();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
 
         ImageButton GK;
         ImageButton computer_science;
@@ -21,5 +33,19 @@ public class CategoryActivity extends AppCompatActivity {
         Aptitude=findViewById(R.id.aptitude);
         History= findViewById(R.id.history);
 
+
+
     }
+
+    private void setupDisplayName(){
+
+        SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+
+        mDisplayName = prefs.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+
+        if (mDisplayName == null) mDisplayName = "Anonymous";
+    }
+
+
+
 }
---
+++ app/src/main/java/com/ankitgaur/ikyu/General_KNowledge_Activity.java
@@ -0,0 +1,13 @@
+package com.ankitgaur.ikyu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class General_KNowledge_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general__knowledge_);
+    }
+}
