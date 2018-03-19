f6e44f26fe79d835f39460e0a398e875efeb5385
ankit gaur <ankit.gaur1023@gmail.com>
2018-03-20 03:00:07 +0530

    GK quizz Done

--- app/src/main/java/com/ankitgaur/ikyu/CategoryActivity.java
+++ app/src/main/java/com/ankitgaur/ikyu/CategoryActivity.java
@@ -1,12 +1,18 @@
 package com.ankitgaur.ikyu;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CategoryActivity extends AppCompatActivity {
 
@@ -23,15 +29,23 @@ public class CategoryActivity extends AppCompatActivity {
         mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
 
-        ImageButton GK;
-        ImageButton computer_science;
-        ImageButton Aptitude;
-        ImageButton History;
+        Button GK;
+        Button computer_science;
+        Button Aptitude;
+        Button science;
 
-        GK= findViewById(R.id.GK);
-        computer_science= findViewById(R.id.compture_science);
-        Aptitude=findViewById(R.id.aptitude);
-        History= findViewById(R.id.history);
+        GK= (Button) findViewById(R.id.b2);
+        computer_science= findViewById(R.id.b1);
+        Aptitude=findViewById(R.id.b4);
+       science= findViewById(R.id.b3);
+
+        GK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent= new Intent(CategoryActivity.this, General_KNowledge_Activity.class);
+                startActivity(myIntent);
+            }
+        });
 
 
 
@@ -45,6 +59,8 @@ public class CategoryActivity extends AppCompatActivity {
 
         if (mDisplayName == null) mDisplayName = "Anonymous";
     }
+
+
 
 
 
